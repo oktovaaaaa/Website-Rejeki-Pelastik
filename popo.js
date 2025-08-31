7451108aa7b9f16ec562172f033ead01228308aa
Oktova Yoga Praditia Samosir <oktovaaaaa@gmail.com>
2025-08-31 16:36:54 +0700

    Penambahan Dataset Produk

--- popo.js
+++ popo.js
@@ -13,12 +13,33 @@
             // Sample product data
             const products = [
                 // {id: 1, name: " PaperLunch Box isi 50 pcs", brand: "P K", price: 10719, originalPrice: 39260, discount: 68, image: "makanan/paper lunch box 160x90x50mm.jpg", category: "makanan", description: "Paper Lunch Box warna Cokelat Size 160 x 90 x 50.", freeItem: "Gratis Microphone"},
-                {id: 1, name: " PaperLunch Box isi 50 pcs", brand: "P K", price: 10719, image: "makanan/paper lunch box 160x90x50mm.jpg", category: "makanan", description: "Paper Lunch Box warna Cokelat Size 160 x 90 x 50."},
-                {id: 2, name: "Poper Bowl 650 ml", brand: "Nesto", price: 49999, image: "makanan/nesto poper bowl 650ml, 25pcs.jpg", category: "makanan", description: "Poper bowl 650 ml isi 25 pcs berwarna putih."},
-                {id: 3, name: "Kantong Plastik Roll Isi Ulang", brand: "PlastikPlus", price: 15000, image: "plastik//cap aladin sz24 cm.jpg", category: "kantong-plastik",},
+                {id: 1, name: " PaperLunch Box isi 50 pcs", brand: "P K", price: 45000, image: "makanan/paper lunch box 160x90x50mm.jpg", category: "makanan", description: "Paper Lunch Box warna Cokelat Size 160 x 90 x 50."},
+                {id: 2, name: "Poper Bowl 650 ml + tutup", brand: "Nesto", price: 35000, image: "makanan/nesto poper bowl 650ml, 25pcs.jpg", category: "makanan", description: "Poper bowl 650 ml isi 25 pcs berwarna putih."},
+                {id: 3, name: "Kantong Plastik cap Aladin Uk: 15,19,24,27,32", brand: "Cap Aladin", price: 12000, image: "plastik//cap aladin sz24 cm.jpg", category: "kantong-plastik",},
                 {id: 4, name: "Cup premium U Cup", brand: "U Cup", price: 75000, image: "minuman//cup dhivers isi 50pcs.jpg", category: "minuman", description: "Cup minuman transparan isi 50 pcs berbahan premium."},
                 {id: 5, name: "Max Creamer satuan", brand: "MaxCreamer", price: 41000, image: "bahan masakan//MaxCreamer satuan.jpg", category: "bahan-masakan", description: "MaxCreamer satuan untuk cream"},
-                {id: 6, name: "Max Creamer 1 karton", brand: "MaxCreamer", price: 936000, image: "bahan masakan//MaxCreamer kotak.jpg", category: "bahan-masakan", description: "MaxCreamer per karton isi 24 kotak untuk membuat cream"},
+                {id: 6, name: "Max Creamer 1 karton", brand: "MaxCreamer", price: 936000, image: "bahan masakan//MaxCreamer kotak.jpg", category: "bahan-masakan", description: "MaxCreamer per karton isi 24 kotak untuk membuat cream"},               
+                {id: 7, name: "Susu Evaporasi 1 karton", brand: "Sunbay", price: 748000, image: "bahan masakan//SunbayEvaporated.jpg", category: "bahan-masakan", description: "Susu Evaporated isi 48"},
+                {id: 8, name: "Mamayo 1 karton", brand: "Mamayo", price: 336000, image: "bahan masakan//mamayo kotak.jpg", category: "bahan-masakan", description: "Mamayo isi 12"},
+                {id: 9, name: "Plastik cap Aladin Jumbo Hitam Uk 45", brand: "Cap Aladin", price: 25000, image: "plastik//cap aladin jumbo sz 45cm.jpg", category: "kantong-plastik",description: "Pelastik berwarna hitam jumbo dengan ukuran 45 cm"},
+                {id: 10, name: "Plastik cap Aladin Jumbo Hitam Uk 45 (Satu Goni)", brand: "Cap Aladin", price: 600000, image: "plastik//cap aladin jumbo sz 45cm.jpg", category: "kantong-plastik",description: "Pelastik berwarna hitam jumbo dengan ukuran 45 cm isi 25 bungkus"},
+                {id: 11, name: "Plastik cap Aladin Jumbo Warna Uk 45", brand: "Cap Aladin", price: 26000, image: "plastik/cap aladin jumbo sz 45cm berwarna.jpg", category: "kantong-plastik",description: "Pelastik berwarna, jumbo dengan ukuran 45 cm "},
+                {id: 12, name: "Plastik Transparan cap Tehko Uk: 15,18,24", brand: "Cap Tehko", price: 8000, image: "plastik/Plastik cap tehko sz15,18,24.jpg", category: "kantong-plastik",description: "Pelastik transparan dengan ukuran: 15cm, 18cm dan 24cm"},
+                {id: 13, name: "Plastik Transparan cap Dayana Uk: 15", brand: "Dayana", price: 3000, image: "plastik/dayana sz 15.jpg", category: "kantong-plastik",description: "Pelastik transparan asoy dengan ukuran 15"},
+                {id: 14, name: "Plastik Transparan cap Melati Uk: 15", brand: "Melati", price: 3500, image: "plastik/melati sz 15.jpg", category: "kantong-plastik",description: "Pelastik transparan asoy dengan ukuran: 15cm"},
+                {id: 15, name: "Plastik Transparan HD/PE Uk: B-28", brand: "KMPP", price: 32000, image: "plastik/Tas HD PE sz 280 450 t500.jpg", category: "kantong-plastik",description: "Pelastik transparan HD dengan ukuran: B-28 280/450mm x 500mm"},
+                {id: 16, name: "Plastik Transparan HD/PE Uk: B-35", brand: "KMPP", price: 8, image: "plastik/tas hd pe sz 350 510 T550.jpg", category: "kantong-plastik",description: "Pelastik transparan HD dengan ukuran: B-35 350/510 x 550mm"},
+                {id: 17, name: "Plastik Transparan HD Cap Matahari Uk:e 35", brand: "Cap Matahari", price: 8, image: "plastik/tas hd cap matahari sz 35.jpg", category: "kantong-plastik",description: "Pelastik putih HD dengan ukuran: 35cm"},
+                {id: 18, name: "Plastik Loundry sz: 60x100, 50x75, 45x65, 40x60, 35x50", brand: "Cap Bengkuang", price: 34000, image: "plastik/laundry cap bengkuang .jpg", category: "kantong-plastik",description: "Pelastik transparan Loundry dengan ukuran: 60x100, 50x75, 45x65, 40x60, 35x50"},
+                {id: 12, name: "Kantongan Transparan HD Number One Uk: 27", brand: "Super number One", price: 8000, image: "plastik/kantong hd number one sz 27.jpg", category: "kantong-plastik",description: "Pelastik transparan HD Super number One dengan ukuran: 27cm berat 500 gram"},
+                {id: 12, name: "Tas HD Biru Steamboat Uk: 18", brand: "Steamboat", price: 8, image: "plastik/Plastik cap tehko sz15,18,24.jpg", category: "kantong-plastik",description: "Pelastik Biru dengan ukuran: S-18, 180/300x370 berat 500 gram"},
+                {id: 12, name: "Tas HD Hijau Steamboat Uk: 15", brand: "Steamboat", price: 8, image: "plastik/tas hd steamboat kuning 150 250 x330.jpg", category: "kantong-plastik",description: "Pelastik Hijau dengan ukuran: SS-15, 150/250 x 330mm berat 500 gram"},
+                {id: 12, name: "Pelastik HD Cap Bawang Uk: 13 x 21", brand: "Cap Bawang", price: 8000, image: "plastik/pelastik hd cap bawang .jpg", category: "kantong-plastik",description: "Pelastik transparan HD Cap Bawang dengan ukuran: 13 X 21cm "},
+                {id: 12, name: "Plastik Transparan Lotus HD Uk: 22 x 35", brand: "Lotus", price: 8000, image: "plastik/lotus 22 x 35.jpg", category: "kantong-plastik",description: "Pelastik transparan HD dengan ukuran: 22 x 35cm"},
+                {id: 12, name: "Plastik Transparan HD Istana Laut Uk: 17x21, 15x21, 12x17", brand: "Istana Laut", price: 8000, image: "plastik/istana laut HD 17 x25, 15x21,12x17.jpg", category: "kantong-plastik",description: "Pelastik transparan HD dengan ukuran: 17x21, 15x21, 12x17"},
+                {id: 12, name: "Plastik Transparan HD cap Tehko Uk: 18x25, 15x21", brand: "Cap Tehko", price: 8, image: "plastik/HD cap Tehko sz18x25, 15x21.jpg", category: "kantong-plastik",description: "Pelastik transparan HD Tehko dengan ukuran: 18 x 25cm dan 15 x 21cm"},
+
+
             ];
 
             let cart = JSON.parse(localStorage.getItem('cart')) || [];
